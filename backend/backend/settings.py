b22528178c9e7d3df2b21129277ed8964b9e1f3d
Kirsten Charles Domingo <kirstencharlespdomingo@iskolarngbayan.pup.edu.ph>
2024-08-29 21:08:53 +0800

    changes to directory

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,3 +147,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+
+# CORS React component
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5713",
+]
