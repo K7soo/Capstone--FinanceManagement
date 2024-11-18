92afdc222b6bb72aab978ef9474bbb46c77f656f
John Loyd Cabral <145852340+R0ck0t0@users.noreply.github.com>
2024-11-18 22:14:57 +0800

    Added a sidebar component transaction type

--- backend/api/views/views_.py
+++ backend/api/views/views_.py
@@ -24,6 +24,9 @@ def journal_temp_view(request):
 def transaction_inbox_view(request):
     return render(request, 'trinbox.html')
 
+def transaction_type_view(request):
+    return render(request, 'trtype.html')
+
 def journal_entries_view(request):
     return render(request, 'journalentries.html')
 
