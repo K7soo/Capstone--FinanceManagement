b334a470bd09f73cd66e3bcc0056b6bd9e68f794
John Loyd Cabral <145852340+R0ck0t0@users.noreply.github.com>
2024-12-04 22:48:48 +0800

    Added Journal Entries Files

---
+++ backend/api/static/js/journalentries.js
@@ -0,0 +1,13 @@
+// Function to get CSRF token from the browser's cookies
+function getCsrfToken() {
+    let csrfToken = null;
+    if (document.cookie && document.cookie !== '') {
+        document.cookie.split(';').forEach(cookie => {
+            const trimmedCookie = cookie.trim();
+            if (trimmedCookie.startsWith('csrftoken=')) {
+                csrfToken = trimmedCookie.split('=')[1];
+            }
+        });
+    }
+    return csrfToken;
+}
