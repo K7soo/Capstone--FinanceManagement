d63b18503266d6e1d34cd748155c19e6d1da0582
Kirsten Charles Domingo <kirstencharlespdomingo@iskolarngbayan.pup.edu.ph>
2025-02-04 07:00:09 +0800

    comment

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 
         # URLs for Integration with External Systems
         path('total-income/', views_api.IncomeTotalWithSources.as_view(), name='income-total'),
-        path('total-income-span/', views_api.IncomeTotalQueryView.as_view(), name='income-totals'),
+        # path('total-income-span/', views_api.IncomeTotalQueryView.as_view(), name='income-totals'), # not working #
 
         path('total-expense/', views_api.ExpenseTotalWithSources.as_view(), name='expense-total'),
     
